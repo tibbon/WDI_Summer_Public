b7d03c03f60ca73647ad918bcfa75f0ee5919bae
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-06-27 18:29:52 -0400

    updated address_book.rb

--- examples/databases/address_book.rb
+++ examples/databases/address_book.rb
@@ -1,32 +1,51 @@
+# This code establishes a connection to postgres and let's us read
+# and write to the database in ruby from the terminal
+# use this code to create an address book in sinatra
+# create an input form
+# add a person
+# list people
 require 'pg'
 require 'pry'
 
+# remember puts prints to the terminal
+# sort of like the equivalent of instance varialbes in html
 puts "Hey gurl"
 
 # get all the inputs
-# put them in the string
-# make it work
+# put them in a string with string interpolation
+puts "what's your first name?"
+first = gets.chomp
 
-# this establishes a connection to the database
-# db = PG.connect(:dbname => 'address_book',
-#   :host => 'localhost')
-# executing sql code
-# passing a string of sql to the database
+puts "what's your last name?"
+last = gets.chomp
 
-# insert into database
-db = PG.connect(:dbname => 'address_book',
-  :host => 'localhost')
+puts "how old are you?"
+age = gets.chomp.to_i
 
-sql = "insert into contacts (first, last, age, gender, dtgd, phone) values (, 'israeli', 26, 'm', true, '310-923-2319')"
+puts "what's your gender? m/f/o?"
+gender = gets.chomp
 
+puts "are you down to get down? t/f"
+dtgd = gets.chomp
+
+puts "cool what's your number?"
+phone = gets.chomp
+
+# establishes a connection to the database
+db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+
+# stores some sql code in a variable
+sql = "insert into contacts (first, last, age, gender, dtgd, phone) values ('#{first}', '#{last}', #{age}, '#{gender}', '#{phone}')"
+# execute the sql code
 db.exec(sql)
-db.close
 
-# reads from database
-# sql = "select first, age from contacts"
-# db.exec(sql) do |result|
-#   result.each do |row|
-#     binding.pry
-#   end
-# end
-# db.close
+# Show the contacts
+sql = "select first, age from contacts"
+# we iterate through the results
+db.exec(sql) do |result|
+  result.each do |row|
+    puts row
+  end
+end
+# always close the connection
+db.close
