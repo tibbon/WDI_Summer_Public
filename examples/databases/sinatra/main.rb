62f364865ef6b8a7ecf2c1927a75c7afe4193088
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-06-28 16:01:16 -0400

    add edit functionality

--- examples/databases/sinatra/main.rb
+++ examples/databases/sinatra/main.rb
@@ -8,6 +8,19 @@ require 'pry'
 # for colors in our terminal
 require 'rainbow'
 
+# helpers do
+#   def open_db
+#     PG.connect(:dbname => 'address_book', :host => 'localhost')
+#   end
+# end
+
+def run_sql(sql)
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  result = db.exec(sql)
+  db.close
+  result
+end
+
 
 get '/' do
   # this prints to sinatra's server logs in terminal
@@ -17,10 +30,10 @@ end
 
 # shows all contacts
 get '/contacts' do
-  puts "we're connecting to the database".color(:blue)
   db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  puts "we're connecting to the database".color(:blue)
   sql = "SELECT * FROM contacts"
-  @contacts = db.exec("SELECT * FROM contacts")
+  @contacts = db.exec(sql)
   db.close
   puts "database is closed".color(:magenta)
   erb :contacts
@@ -44,6 +57,15 @@ post '/contacts/delete' do
   # go someplace
 end
 
+get '/contacts/:id/edit' do
+  id = params[:id]
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  sql = "SELECT * FROM contacts WHERE id = #{id}"
+  @contact = db.exec(sql).first
+  db.close
+  erb :edit
+end
+
 # Make a new contact
 get '/contacts/new' do
   erb :form
@@ -57,4 +79,16 @@ get '/contacts/:id' do
   @contact = db.exec(sql).first
   db.close
   erb :contact
+end
+
+post '/contacts/:id' do
+  id = params[:id]
+  first = params[:first]
+  last = params[:last]
+  age = params[:age]
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  sql = "UPDATE contacts SET (first, last, age) = ('#{first}', '#{last}',#{age}) WHERE id = #{id}"
+  db.exec(sql)
+  db.close
+  redirect to '/contacts'
 end
