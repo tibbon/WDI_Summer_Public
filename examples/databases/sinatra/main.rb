770234e227edb922ed2c8ebc7e6717857b274e57
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-06-28 10:55:20 -0400

    finished basic sinatra

--- examples/databases/sinatra/main.rb
+++ examples/databases/sinatra/main.rb
@@ -8,17 +8,38 @@ require 'pry'
 # for colors in our terminal
 require 'rainbow'
 
+
 get '/' do
+  # this prints to sinatra's server logs in terminal
+  puts "This is the index".color(:red)
   erb :index
 end
 
 # shows all contacts
 get '/contacts' do
+  puts "we're connecting to the database".color(:blue)
   db = PG.connect(:dbname => 'address_book', :host => 'localhost')
   sql = "SELECT * FROM contacts"
-  @contacts = db.exec(sql)
+  @contacts = db.exec("SELECT * FROM contacts")
   db.close
+  puts "database is closed".color(:magenta)
   erb :contacts
+end
+
+post '/contacts' do
+  first = params[:first]
+  last = params[:last]
+  age = params[:age]
+  sql = "INSERT INTO CONTACTS (first, last, age) VALUES ('#{first}', '#{last}', #{age})"
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  db.exec(sql)
+  db.close
+  redirect to '/contacts'
+end
+
+# Make a new contact
+get '/contacts/new' do
+  erb :form
 end
 
 # show one specific contact
