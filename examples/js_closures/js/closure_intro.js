02f0c3731984256690d2ba4e6724b16f83bfd395
Clearfield Christopher <chris.clearfield@gmail.com>
2013-07-24 15:28:11 -0400

    Formatting to satisfy Phil

--- examples/js_closures/js/closure_intro.js
+++ examples/js_closures/js/closure_intro.js
@@ -1,13 +1,13 @@
 function handleStudents(){
-    var i = 0;  // scope: local variable, type: number
-    var students = ['Jim', 'Bob', 'Tracy', 'Justin', 'Emily'];  // s: local, t:???
-    return function(){  // from SO: the inner function has access to the 
-			// environment it was created within 
-			// even after the code that created the environment has returned.
-	var student =  students[i]; // s: ???, t: string
-	i+=1; 
-	return student; // What type is this returning? 
-    }
+  var i = 0;  // scope: local variable, type: number
+  var students = ['Jim', 'Bob', 'Tracy', 'Justin', 'Emily'];  // s: local, t:???
+  return function(){  // from SO: the inner function has access to the 
+    // environment it was created within 
+    // even after the code that created the environment has returned.
+    var student =  students[i]; // s: ???, t: string
+    i+=1; 
+    return student; // What type is this returning? 
+  }
 }
 
 // What type of variable is nextStudent?
@@ -16,16 +16,16 @@ var nextStudent = handleStudents();
 
 // onload
 window.onload = function(){
-    // Adding an event handler to our addStudent button
-    document.getElementById("addStudent").onclick = function(event){
-	document.getElementById('students').innerHTML += nextStudent() + '<br/>\n';
-    };
+  // Adding an event handler to our addStudent button
+  document.getElementById("addStudent").onclick = function(event){
+    document.getElementById('students').innerHTML += nextStudent() + '<br/>\n';
+  }
 
-    document.getElementById("clearStudents").onclick = function(event){
-	document.getElementById('students').innerHTML = '' 
-    }
+  document.getElementById("clearStudents").onclick = function(event){
+    document.getElementById('students').innerHTML = '' 
+  }
 
-    document.getElementById("resetClosure").onclick = function(event){
-	nextStudent = handleStudents(); 
-    };
+  document.getElementById("resetClosure").onclick = function(event){
+    nextStudent = handleStudents(); 
+  };
 };
