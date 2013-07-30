023a0e199ba6a1a42615c1bfa2d3566a0679f915
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-07-30 11:08:11 -0400

    add memory student

---
+++ assignments/memory-student/README.md
@@ -0,0 +1,27 @@
+## Memory Lab
+
+### Goal:
+* Practice using jQuery DOM manipulation
+* Pratice using jQuery Events
+* Create a timer
+
+### The Memory Game:
+* Memory is a card game in which all of the cards are laid face down on a surface.
+* Two cards may be revealed at a time.
+* The object of the game is to turn over pairs of matching cards.
+
+### To do:
+* Get the game to populate the DOM with 10 cards when the "small" button is clicked.
+* Make sure each card is assigned a unique ID so it can be mapped to a letter.
+* Implement click functionality so that when a player clicks on the card, it reveals the letter.
+* Add logic that compares the cards to try to find a match
+* Make sure if you click the same card twice it doesn't mistakenly think it is a match!
+* Add styling for "found", "won", and "hover"
+* Give the player the option to play a game that is small, medium or large.
+* Add a game timer. It should stop when the player wins.
+
+### Bonus:
+* How can you programatically create the letter arrays?
+* Are there ways to refactor your code to make it DRYer?
+* Animate the cards when they are flipped over.
+* Can you implement the memory game with images instead of letters?
---
+++ assignments/memory-student/memory.js
@@ -0,0 +1,51 @@
+//Global Variables
+
+//Time that increments in the game
+var timerId = 0,
+    time = 0;
+
+//arrays with letters in them.
+var lettersSmall  = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E'],
+    lettersMedium = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E',
+                     'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J'],
+    lettersLarge  = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E',
+                     'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J',
+                     'K', 'K', 'L', 'L', 'M', 'M', 'N', 'N', 'O', 'O',
+                     'P', 'P', 'Q', 'Q', 'R', 'R', 'S', 'S', 'T', 'T'];
+
+//array of letters. will change based on size of game.
+var letters;
+
+//last card/letter you clicked on. comes from the letter divs.
+var lastId = '',
+    lastCard = '';
+
+//Code In Here gets executed once code is ready. ie hovering, clicking events//
+$(function() {
+
+});
+
+// Initializes the game and creates the board
+function startGame() {
+
+}
+
+// Flips a card and checks for a match
+function cardClick() {
+
+}
+
+//Add hoverclass to cards.
+function hovering() {
+
+}
+
+//Start the timer
+function startTime() {
+
+}
+
+//Increment the timer and display the new time
+function updateTime() {
+
+}
