c075eab997703fd0aadebc4a714dafccf4c9ef4f
David Fisher <tibbon+github@gmail.com>
2013-08-15 11:03:44 -0400

    Added our weapon model

--- examples/backbone_weaponry/main.js
+++ examples/backbone_weaponry/main.js
@@ -1,5 +1,13 @@
 // MODEL: Weapon
-
+var Weapon = Backbone.Model.extend({
+	idAttribute: 'slug',
+	defaults: {
+		name: "Aphrodite\'s Sword",
+		type: "Sword",
+		slug: "aphrodites-sword",
+		number_slain: 9001
+	}
+})
 // COLLECTION: Weapons
 
 // ROUTER: Everything
