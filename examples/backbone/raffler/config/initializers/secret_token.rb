bd34f222a543c4e4b77c4508b91bed3099e191b6
David Fisher <tibbon+github@gmail.com>
2013-08-16 09:59:03 -0400

    Ran controller generator to create Main controller with an action of Index. Added Loading text to app views main index

---
+++ examples/backbone/raffler/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = 'f77b9e86150135ce9747b3aa1d9f5ba24fcdf549abc5f4fa22c6f45eba92d81eb10b36181633e98f1c3df6b3484f4bd1adb31e9d764c8976a9f5263c87bb3519'
