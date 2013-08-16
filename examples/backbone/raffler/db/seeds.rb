2faa73ca0fb3a3ab94fd473d34767b7dec2cb38b
David Fisher <tibbon+github@gmail.com>
2013-08-16 11:11:33 -0400

    Added Seeds

--- examples/backbone/raffler/db/seeds.rb
+++ examples/backbone/raffler/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Entry.create!(name: "Matz")
+Entry.create!(name: "Yehuda Katz")
+Entry.create!(name: "DHH")
+Entry.create!(name: "Jose Valim")
+Entry.create!(name: "Dr Nic")
+Entry.create!(name: "John Nunemaker")
+Entry.create!(name: "Aaron Patterson")
