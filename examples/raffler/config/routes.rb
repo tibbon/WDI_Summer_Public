a513733a0ead9786f688e26482d8878871138a3b
David Fisher <tibbon+github@gmail.com>
2013-08-15 22:51:56 -0400

    APi layer working

--- examples/raffler/config/routes.rb
+++ examples/raffler/config/routes.rb
@@ -1,4 +1,9 @@
 Raffler::Application.routes.draw do
+  scope "api" do
+    resources :entries
+  end
+
+
   root to: "main#index"
 
   # The priority is based upon order of creation:
--- examples/raffler/db/seeds.rb
+++ examples/raffler/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Entry.create!(name: "Matz")
+Entry.create!(name: "Yehuda Katz")
+Entry.create!(name: "DHH")
+Entry.create!(name: "Jose Valim")
+Entry.create!(name: "Dr Nic")
+Entry.create!(name: "John Nunemaker")
+Entry.create!(name: "Aaron Patterson")
