dd7e399c6d020f419c08664b3c734ea569c59bd3
David Fisher <tibbon+github@gmail.com>
2013-08-16 10:33:28 -0400

    Show Router view is working for alert

--- examples/backbone/raffler/app/assets/javascripts/routers/entries_router.js
+++ examples/backbone/raffler/app/assets/javascripts/routers/entries_router.js
@@ -1,8 +1,12 @@
 Raffler.Routers.Entries = Backbone.Router.extend({
 	routes: {
-		'': 'index'
+		'': 'index',
+		'entries/:id': 'show'
 	},
 	index: function() {
 		alert("Hello from Router Index");
+	},
+	show: function(id) {
+		alert("Hello from Router Show Number:" + id);
 	}
 });
