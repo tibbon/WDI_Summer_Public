14eeabd1c811dfd3b4ce0d76058ae89c42829ab8
David Fisher <tibbon+github@gmail.com>
2013-08-16 11:44:19 -0400

    Loading people dynamically from the server

--- examples/backbone/raffler/app/assets/javascripts/routers/entries_router.js
+++ examples/backbone/raffler/app/assets/javascripts/routers/entries_router.js
@@ -3,8 +3,12 @@ Raffler.Routers.Entries = Backbone.Router.extend({
 		'': 'index',
 		'entries/:id': 'show'
 	},
+	initialize: function(){
+		this.collection = new Raffler.Collections.Entries();
+		this.collection.fetch({reset: true}); // Will explain in a second
+	},
 	index: function() {
-		var view = new Raffler.Views.EntriesIndex();
+		var view = new Raffler.Views.EntriesIndex({ collection: this.collection });
 		$('#container').html(view.render().el);
 	},
 	show: function(id) {
--- examples/backbone/raffler/app/assets/javascripts/views/entries/entries_index.js
+++ examples/backbone/raffler/app/assets/javascripts/views/entries/entries_index.js
@@ -1,7 +1,10 @@
 Raffler.Views.EntriesIndex = Backbone.View.extend({
   template: HandlebarsTemplates['entries/index'],
+  initialize: function(){
+  	this.collection.on('reset', this.render, this);
+  },
   render: function() {
-  	$(this.el).html(this.template({entries: "Always look on the bright side of life"}));
+  	$(this.el).html(this.template({collection: this.collection}));
   	return this;
   }
 });
