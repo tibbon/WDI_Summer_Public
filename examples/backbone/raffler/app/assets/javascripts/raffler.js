39081f05b1e7506078fd256b853445c8fb1c9dad
David Fisher <tibbon+github@gmail.com>
2013-08-16 10:05:19 -0400

    Ran backbone installer

---
+++ examples/backbone/raffler/app/assets/javascripts/raffler.js
@@ -0,0 +1,13 @@
+window.Raffler = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Raffler.initialize();
+});
