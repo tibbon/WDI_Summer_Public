d8f2e39b2db78a577a02286726464a96360ca57f
David Fisher <tibbon+github@gmail.com>
2013-08-21 14:46:24 -0400

    ran: rails _3.2.13_ new mongoTakeTwo --skip-active-record

---
+++ examples/deployment/mongoTakeTwo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoTakeTwo::Application.config.secret_token = 'c3e3c7e2e8f922763ce4fc828413a21746ba868d08e375dd0bcfadd9a4168ce882ac0f468025609e482a8874faf59b7455b6a8f91ab862c35893810f205cfc88'
