434d1ad8f10f7df9288adee0b6e94888e3c606e3
David Fisher <tibbon+github@gmail.com>
2013-08-22 16:30:53 -0400

    Initial state for 3.2

---
+++ examples/TransferToFour/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TransferToFour::Application.config.secret_token = '5129154f59f41d613b6e821ba0a0fab9dc17d545072b9a5e7b7f4fd8cd0db832a2f4c2e032f29308b15cf3e16b111eeba0a49e8d3b45e91516c5b38b51178ba4'
