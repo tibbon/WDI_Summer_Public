11e2f54fd99c8dfc3bf24a17696b11b81c0cceb5
David Fisher <tibbon+github@gmail.com>
2013-08-23 14:25:31 -0400

    Scaffolded products

---
+++ examples/ViewCacheStore/app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :name, :description, :created_at, :updated_at
--- examples/ViewCacheStore/config/routes.rb
+++ examples/ViewCacheStore/config/routes.rb
@@ -1,4 +1,6 @@
 ViewCacheStore::Application.routes.draw do
+  resources :products
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
