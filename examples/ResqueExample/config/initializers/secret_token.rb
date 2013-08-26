dedff89079825952db17f52e623e5143c433ae60
David Fisher <tibbon+github@gmail.com>
2013-08-26 15:30:02 -0400

    Started on basic Resque example

---
+++ examples/ResqueExample/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResqueExample::Application.config.secret_key_base = 'aa9740ee65fce18a43c48fbb2f209c118256d69cd90d6de7c2e9f37889ff684fcc929e9c9791b29c020a4e339a9cc0fc106a7bc396f1d722cb6d189481cbbc7b'
