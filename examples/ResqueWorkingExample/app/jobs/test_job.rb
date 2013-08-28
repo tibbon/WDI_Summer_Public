fbe5f96b5f55c55887b3a2af3558256c29125808
David Fisher <tibbon+github@gmail.com>
2013-08-28 16:01:02 -0400

    Created example_job.rb with an ExampleJob module in it

---
+++ examples/ResqueWorkingExample/app/jobs/test_job.rb
@@ -0,0 +1,8 @@
+module TestJob
+  @queue = :default
+
+  def self.perform
+  	puts "This job happened in the background outside of Rails."
+  	puts "Any code put here has access to the Rails environment."
+  end
+end
